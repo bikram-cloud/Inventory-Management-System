1fd8ecd71579498b22424d3e491f73749fdbcc20
bikram-cloud <bikramnp43@gmail.com>
2019-09-26 21:51:08 +0900

    My Project

---
+++ database/constants.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+define("HOST","localhost");
+define("USER","root");
+define("PASS","root");
+define("DB","project_inventory");
+
+define("DOMAIN","http://localhost:8888/inventory_management_system");
+
+?>
